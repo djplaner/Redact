94f0f0aaf321240c37f88203e6c25167eefd265d
David Jones <davidthomjones@gmail.com>
2016-06-23 10:16:25 +1000

    Modified to fit at USQ
    Removed some CQU specific systems and added the odd extra bit for USQ

--- Redact.user.js
+++ Redact.user.js
@@ -1,21 +1,23 @@
 // ==UserScript==
 // @name        Redact
-// @namespace   https://damos.world
+// @namespace   http://damos.world
 // @description Redact parts of a web page for privacy such as live demos
+// @include     http://usqstudydesk.usq.edu.au/m2/*
+// @include     https://usqstudydesk.usq.edu.au/m2/*
 // @grant       GM_addStyle
 // @run-at      document-start
 // @version     1.0.0
 // ==/UserScript==
+// Quick and dirty update to work better with USQ systems
 
 try
 {
-  console.log("Hiding body") ;
+//  console.log("Hiding body") ;
   GM_addStyle("body {visibility: hidden;}") ;
-  console.log("Body hidden") ;
+//  console.log("Body hidden") ;
 
   var func = function()
   {
-    //Moodle LMS CSS for redaction
     var redact_text = 
     [
       '.useridnumber, .useremail, .idnumber, .email, .subfield_firstname, .subfield_idnumber, .subfield_email',
@@ -31,24 +33,25 @@ try
       'table#participants tbody tr td:nth-child(4), table#participants tbody tr td:nth-child(5)',
       'span.ui-id-1',
       'table.studentInfo tbody tr:nth-child(3) td',
-      'table.studentAccess tbody tr td:nth-child(1), table.studentAccess tbody tr td:nth-child(2), table.studentAccess tbody tr td:nth-child(3)'
+      'table.studentAccess tbody tr td:nth-child(1), table.studentAccess tbody tr td:nth-child(2), table.studentAccess tbody tr td:nth-child(3)',
     ] ;
 
-    //Moodle LMS CSS for redaction
     var redact_image =
     [
-      '.userpicture'
+      '.picture',
+      '.userpicture',
+      'img[src*="studentphoto.cqu.edu.au"]' /*handbook photo*/
     ] ;
 
-    console.log("Redacting text") ;
+//    console.log("Redacting text") ;
     GM_addStyle(redact_text.join()+' {color: transparent !important; text-shadow: rgba(0, 0, 0, 0.5) 0px 0px 6px !important;}') ;
-    console.log("Text Redacted") ;
+ //   console.log("Text Redacted") ;
 
-    console.log("Redacting Images") ;
+  //  console.log("Redacting Images") ;
     GM_addStyle(redact_image.join()+' {filter: blur(10px) !important;}') ;
-    console.log("Images Redacted") ;
+   // console.log("Images Redacted") ;
 
-    console.log("Setting page visible") ;
+//    console.log("Setting page visible") ;
     window.document.getElementsByTagName("body")[0].style.visibility = "visible" ;
   }
 
